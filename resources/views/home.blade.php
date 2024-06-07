7b90830f3513195788a52ebf14c0270c03999d60
justinbondocponce <ponce.justin16@gmail.com>
2024-06-07 14:52:36 +0800

    hi

---
+++ resources/views/home.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">{{ __('Dashboard') }}</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    
+
+        <!-- Main content -->
+        <section class="content">
+        <div class="container-fluid">
+        <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>BSIT-3B EXAMINERS</h3>
+
+                <p></p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+          
+
+       
+
+      
+          <!-- ./col -->
+        </div>
+
+        <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
